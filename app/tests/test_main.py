6eaa52a8e75e37fb2cd85f72ec9f7146b007d9da
carlahauu <carlahau8@gmail.com>
2025-12-24 00:53:50 -0800

    test: Add conftest fixture for TestClient

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,11 +1,4 @@
-import pytest
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
