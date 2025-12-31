af0ca312488ffc3c9beadf3ebc795eda8e752c80
carlahauu <carlahau8@gmail.com>
2025-12-30 23:43:50 -0800

    feat: Add contact us page and cypress e2e tests for footer

---
+++ frontend/cypress/e2e/footer.cy.ts
@@ -0,0 +1,62 @@
+describe('Footer', () => {
+  const footerItems = [
+    'Privacy Policy',
+    'Terms of Service',
+    'Contact Us',
+    'GitHub Repo',
+    'Made with love by Carla Hau',
+  ];
+
+  beforeEach(() => {
+    cy.visit('localhost:3000');
+  });
+
+  it('should display footer with footer items', () => {
+    cy.get('[data-testid="footer"]')
+      .should('be.visible')
+      .within(() => {
+        footerItems.forEach((item) => {
+          cy.contains(item).should('be.visible');
+        });
+      });
+  });
+
+  it('should navigate between pages via footer', () => {
+    cy.get('[data-testid="footer"]').contains('Privacy Policy').click();
+    cy.url().should('include', '/privacy-policy');
+
+    cy.get('[data-testid="footer"]').contains('Terms of Service').click();
+    cy.url().should('include', '/terms-of-service');
+
+    cy.get('[data-testid="footer"]').contains('Contact Us').click();
+    cy.url().should('include', '/contact-us');
+
+    cy.get('[data-testid="footer"]').contains('GitHub Repo').click();
+    cy.url().should('eq', 'https://github.com/carlahauu/Santa-Match');
+
+    cy.go('back');
+
+    cy.get('[data-testid="footer"]').contains('Carla Hau').click();
+    cy.url().should('eq', 'https://www.carlahau.com/');
+  });
+
+  it('should persist footer across pages', () => {
+    cy.visit(`localhost:3000/create-group`);
+    cy.get('[data-testid="footer"]').should('be.visible');
+    cy.get('[data-testid="footer"]')
+      .contains('Privacy Policy')
+      .should('be.visible');
+  });
+
+  it('shows footer on mobile', () => {
+    cy.viewport('iphone-xr');
+
+    cy.get('[data-testid="footer"]')
+      .should('be.visible')
+      .within(() => {
+        footerItems.forEach((item) => {
+          cy.contains(item).should('be.visible');
+        });
+      });
+  });
+});
