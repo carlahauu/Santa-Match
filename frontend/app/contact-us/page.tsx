160ae250b8d94b113254b6e18b81a40d9c8b95c0
carlahauu <carlahau8@gmail.com>
2025-12-30 23:51:50 -0800

    style: Update margin on contact us page

--- frontend/app/contact-us/page.tsx
+++ frontend/app/contact-us/page.tsx
@@ -1,7 +1,7 @@
 export default function ContactUs() {
   return (
     <div className="flex flex-col font-sans items-center justify-center">
-      <main className="flex md:w-[60%] lg:w-[40%] w-[85%] flex-col mt-40">
+      <main className="flex md:w-[60%] lg:w-[40%] w-[85%] flex-col mt-40 mb-10">
         <h1 className="text-left text-2xl font-bold mb-6">Contact Us</h1>
         <p>
           Have any questions? Feel free to reach out to: carlahau110@gmail.com
