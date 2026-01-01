dd57644f7de820953efa33091de67d6297c83373
carlahauu <carlahau8@gmail.com>
2026-01-01 02:28:02 -0800

    Add tests for /contact-us page

--- frontend/app/contact-us/page.tsx
+++ frontend/app/contact-us/page.tsx
@@ -2,8 +2,13 @@ export default function ContactUs() {
   return (
     <div className="flex flex-col font-sans items-center justify-center">
       <main className="flex md:w-[60%] lg:w-[40%] w-[85%] flex-col mt-40 mb-10">
-        <h1 className="text-left text-2xl font-bold mb-6">Contact Us</h1>
-        <p>
+        <h1
+          data-testid="contactUsHeading"
+          className="text-left text-2xl font-bold mb-6"
+        >
+          Contact Us
+        </h1>
+        <p data-testid="contactUsContent">
           Have any questions? Feel free to reach out to: carlahau110@gmail.com
         </p>
       </main>
